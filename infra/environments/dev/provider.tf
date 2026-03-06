45a130db11a878c4ee15b4d75f8c387eca51420e
andres <andrespperdomo@gmail.com>
2026-03-05 21:13:56 -0500

    six

--- infra/environments/dev/provider.tf
+++ infra/environments/dev/provider.tf
@@ -1,5 +1,16 @@
 provider "aws" {
+  region                      = "us-east-1"
+  access_key                  = "test"
+  secret_key                  = "test"
 
-  region = "us-east-1"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
 
+  endpoints {
+    dynamodb = "http://localhost:4566"
+    s3       = "http://localhost:4566"
+    sqs      = "http://localhost:4566"
+    sns      = "http://localhost:4566"
+  }
 }
