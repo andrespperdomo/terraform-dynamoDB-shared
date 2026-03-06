e00461d04bd71e4d7987f38e60532a27bb691d27
andres <andrespperdomo@gmail.com>
2026-03-06 02:32:50 -0500

    output upgrade

--- infra/environments/dev/outputs.tf
+++ infra/environments/dev/outputs.tf
@@ -9,3 +9,10 @@ output "billing_table_arn" {
 output "notifications_table_arn" {
   value = module.notifications_table.table_arn
 }
+output "queue_url" {
+  value = aws_sqs_queue.this.id
+}
+
+output "queue_arn" {
+  value = aws_sqs_queue.this.arn
+}
