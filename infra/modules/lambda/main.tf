623512016bf3cbb1a4a1519633a751cbfecd4272
andres <andrespperdomo@gmail.com>
2026-03-08 01:15:05 -0500

    fix it lambda fail

--- infra/modules/lambda/main.tf
+++ infra/modules/lambda/main.tf
@@ -5,11 +5,17 @@ resource "aws_iam_role" "lambda_role" {
     Version = "2012-10-17",
     Statement = [{
       Action = "sts:AssumeRole"
-      Principal = { Service = "lambda.amazonaws.com" }
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
       Effect = "Allow"
-      Sid = ""
     }]
   })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
 resource "aws_lambda_function" "this" {
